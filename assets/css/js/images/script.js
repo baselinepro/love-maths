ff097cb30b8cab82d15a4c1d255966884412f5ed
baselinepro ali <master_ali_786@hotmail.com>
2023-05-11 13:04:26 +0000

    Display addition question to the user

--- assets/css/js/images/script.js
+++ assets/css/js/images/script.js
@@ -9,18 +9,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type"); 
-                alert('You clicked ${gameType}');
+                runGame(gameType);
             }
         })
     }
 })
 
-function runGame() {
-    
+runGame("addition");
+
+function runGame(gameType) {
+
 //creates two random numbers between 1 and 25 
 let num1 = Math.floor(Math.random() * 25) + 1; 
 let num2 = Math.floor(Math.random() * 25) + 1;
 }
+
+if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2); 
+} else {
+    alert('Unknown game type: ${gameType}'); 
+    throw 'Unknown game type: ${gameType}. Aborting!'; 
+} 
+
+}
+
 
 function checkAnswer() {
 
@@ -38,8 +50,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
