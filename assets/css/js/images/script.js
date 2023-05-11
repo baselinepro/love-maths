84445ec6caaa3c274422174e236af3e160a1f503
baselinepro ali <master_ali_786@hotmail.com>
2023-05-11 14:27:21 +0000

    Add code to check the answer and respond to the user

--- assets/css/js/images/script.js
+++ assets/css/js/images/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
+                checkAnswer(); 
             } else {
                 let gameType = this.getAttribute("data-type"); 
                 runGame(gameType);
@@ -35,7 +35,19 @@ if (gameType === "addition") {
 
 
 function checkAnswer() {
+let userAnswer = parseInt(document.getElementById("answer-box").value);
+let calculatedAnswer = calculateCorrectAnswer();
+let isCorrect = userAnswer === calculatedAnswer[0];
+ if (isCorrect) {
+    alert("Hey! you got it right! :D"); 
+ } else {
+    alert('Awwww.... you answered ${userAnswer}. the correct answer was ${calculatedAnswer[0]}!');
 
+ }
+
+ runGame(calculatedAnswer[1]);
+ 
+}
 }
 
 
