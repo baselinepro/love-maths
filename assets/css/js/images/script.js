70ea1e31248d3d37d8538b111866ca7b91c97da5
baselinepro ali <master_ali_786@hotmail.com>
2023-05-11 15:25:05 +0000

    Add multiplication game code

--- assets/css/js/images/script.js
+++ assets/css/js/images/script.js
@@ -26,6 +26,9 @@ let num2 = Math.floor(Math.random() * 25) + 1;
 
 if (gameType === "addition") {
     displayAdditionQuestion(num1, num2); 
+    else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }
 } else {
     alert('Unknown game type: ${gameType}'); 
     throw 'Unknown game type: ${gameType}. Aborting!'; 
@@ -60,7 +63,9 @@ let operator = document.getElementById("operator").innerText;
 
 if (operator === "+"){
     return [operand1 + operand2, "addition"];
-} else {
+} else if (operator === "x") {
+    return [operand1 * operand2, "multiply"];
+ } else {
     alert('Unimplemented operator ${operator}');
     throw 'Unimplemented operator ${operator}. Aborting!';
 }
@@ -89,6 +94,8 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
